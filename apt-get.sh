15f7cfb968addeefd8aa63c53e951babf364003c
strike-art <mail@strike-art.ru>
2015-12-31 00:23:17 +0300

    Create apt-get.sh

---
+++ apt-get.sh
@@ -0,0 +1,1 @@
+apt-get install nano proftpd mysql-server mysql-client apache2-mpm-itk libapache2-mod-php php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
